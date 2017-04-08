9936b1eae65aa126ff654a98b00bdf2ff6d867cb
papy <e-papy@nifty.com>
2017-04-08 15:00:43 +0900

    Add files via upload

---
+++ SQL/t_example.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `t_example` (
+  `no` int(11) NOT NULL AUTO_INCREMENT COMMENT 'NO(自動連番)',
+  `type` varchar(10) NOT NULL COMMENT '種類',
+  `value` int(11) NOT NULL COMMENT '数値',
+  `string` varchar(30) DEFAULT NULL COMMENT '文字列',
+  `reg_date` datetime NOT NULL,
+  PRIMARY KEY (`no`) 
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
