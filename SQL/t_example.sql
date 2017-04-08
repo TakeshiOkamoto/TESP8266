46c34cecfe2a7dfe8626dcf528f14e0314c6e989
papy <e-papy@nifty.com>
2017-04-09 01:23:52 +0900

    Update t_example.sql

--- SQL/t_example.sql
+++ SQL/t_example.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `t_example` (
   `type` varchar(10) NOT NULL COMMENT '種類',
   `value` int(11) NOT NULL COMMENT '数値',
   `string` varchar(30) DEFAULT NULL COMMENT '文字列',
-  `reg_date` datetime NOT NULL,
+  `reg_date` datetime NOT NULL COMMENT '登録日',
   PRIMARY KEY (`no`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
